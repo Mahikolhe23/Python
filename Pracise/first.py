215767ab50997ab55e7fde403ea8702451509b13
Mahendra Kolhe <mahendrakolhe@Mahi-MacBook.local>
2025-01-03 18:50:52 +0530

    python first code

---
+++ Pracise/first.py
@@ -0,0 +1,8 @@
+import logging as lg
+
+lg.info('"""Programming aasan hai. We are going to learn this in depth. While learning we have to make sure that\nwe are implemeting all the logics by ourself. The aim here is to build our \"4 BHK\" house with the\n \thelp of \'Python programming\'. We have total land is of \\100 ft * 100ft /, to colmplete the house\n \twe have total 6 labours with \'different skill set like \"\\\ building wall or building roof \\\\".\n\t\tI have to print this paragraph as it is given here."""')
+
+# print('"""Programming aasan hai. We are going to learn this in depth. While learning we have to make sure that\nwe are implemeting all the logics by ourself. The aim here is to build our \"4 BHK\" house with the\n \thelp of \'Python programming\'. We have total land is of \\100 ft * 100ft /, to colmplete the house\n \twe have total 6 labours with \'different skill set like \"\\\ building wall or building roof \\\\".\n\t\tI have to print this paragraph as it is given here."""')
+
+formatter = lg.Formatter('[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s','%m-%d %H:%M:%S')
+
