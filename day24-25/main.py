79e38f2a318f687eb37e8d218966e622d44869c2
Mahendra Kolhe <mkolhe23@gmail.com>
2025-02-02 22:17:47 +0530

    starting with pandas

--- day24-25/main.py
+++ day24-25/main.py
@@ -1,18 +1,28 @@
-# import csv
-# with open('data.csv') as data_file:
-#     data = csv.reader(data_file)
-#     temp = []
-#     for row in data:
-#         temp.append(row[1])
-
+import turtle
 import pandas
+
+# screen = turtle.Screen()
+# screen.setup(width=600,height=600)
+# screen.title('INDIA')
+# image = 'india_map.gif'
+# screen.addshape(image)
+# turtle.shape(image)
+
 data = pandas.read_csv('data.csv')
-# avg_temp = sum(data['temp'].to_list())/len(data['temp'].to_list())
-# print(avg_temp)
-# print(data['temp'].max())
 
-# print(data[data.temp == data.temp.max()])
+data = data.state.tolist()
+new_data = []
 
-# monday = data[data.day == 'mon']
-# monday.temp = (monday.temp * 9 / 5) + 32
-# print(monday.temp)  
+for d in range(0,len(data)):
+    if d % 2 ==0:
+        new_data.append(data[d])
+
+print(new_data)
+df = pandas.DataFrame({'state':new_data})
+df.to_csv('data.csv')
+
+# screen.exitonclick()
+
+
+
+
