235a94d1474055ebfa3d46afe62df2d263e90fde
Mahendra Kolhe <mkolhe23@gmail.com>
2025-02-04 21:41:36 +0530

    guess state india

--- day24-25/main.py
+++ day24-25/main.py
@@ -1,28 +1,26 @@
 import turtle
 import pandas
 
-# screen = turtle.Screen()
-# screen.setup(width=600,height=600)
-# screen.title('INDIA')
-# image = 'india_map.gif'
-# screen.addshape(image)
-# turtle.shape(image)
+screen = turtle.Screen()
+screen.setup(width=600,height=600)
+screen.title('INDIA')
+image = 'india_map.gif'
+screen.addshape(image)
+turtle.shape(image)
 
+ans_text = screen.textinput(title='Guess the state',prompt='Whats the anathor state name ?').title()
 data = pandas.read_csv('data.csv')
+df = pandas.DataFrame(data=data)
+all_states = df.state.tolist()
 
-data = data.state.tolist()
-new_data = []
+if ans_text in all_states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == ans_text]
+    print(state_data)
+    t.goto(state_data.x.item(), state_data.y.item())
+    t.write(ans_text)
 
-for d in range(0,len(data)):
-    if d % 2 ==0:
-        new_data.append(data[d])
-
-print(new_data)
-df = pandas.DataFrame({'state':new_data})
-df.to_csv('data.csv')
-
-# screen.exitonclick()
-
-
-
+screen.exitonclick()
 
