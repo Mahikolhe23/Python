fb77bbb01038ab0ad9f70cd1e4123eda7ef72849
Mahendra Kolhe <mkolhe23@gmail.com>
2025-05-03 15:13:57 +0530

    pyodbc

--- Pracise/python_db.py
+++ Pracise/python_db.py
@@ -15,10 +15,28 @@ except Exception as e:
 
 con = conn.cursor()
 
-con.execute('SELECT * FROM Emp')
-
+# Read
+con.execute('SELECT top 5 * FROM Emp')
 result = con.fetchall()
+for r in result:
+    print(r)
+
+# Update 
+con.execute("Update emp set first_name = 'Mahi' where employee_id = 198")
+con.execute('SELECT * FROM Emp where employee_id = 198')
+print(con.fetchall())
+
+# Insert
+insert_query = "Insert into emp(first_name, last_name) values(?,?)"
+con.execute(insert_query,('niku1','kolhe1'))
+con.execute("SELECT * FROM Emp where first_name = 'niku1'")
+print(con.fetchall())
 
 
+# delete
+delete_qury = "delete from emp where first_name = 'niku1'"
+con.execute(delete_qury)
+con.execute("SELECT * FROM Emp where first_name = 'niku'")
+print(con.fetchall())
 
 
