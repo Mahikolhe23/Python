418c736a18ce10fd2197d9b70ae4c3655f41b5c3
Mahendra Kolhe <mkolhe23@gmail.com>
2025-06-08 19:36:01 +0530

    practise

---
+++ Practise/python_pandas.py
@@ -0,0 +1,10 @@
+
+n = 9
+m = 27
+# for i in range(1, n, 2):
+#     print(('.|.' * i).center(m,'-'))
+# print('WELCOME'.center(m,'-'))
+for i in range(n-2, 0, -2):
+    print(('.|.'*i).center(m,'-')) 
+
+
